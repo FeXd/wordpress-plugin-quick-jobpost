e6e8deea36211028ed2c1305069b12ec1509dd8e
Arlin Schafffel <arlin@fexd.com>
2024-06-04 11:17:33 -0600

    Create Plugin Base File

---
+++ quick-jobpost.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+Plugin Name: Quick Jobpost
+Plugin URI: https://github.com/FeXd/wordpress-plugin-quick-jobpost
+Description: A simple WordPress plugin to help facilitate posting to my job weblog at https://arlin.education/jobs.
+Version: 0.0.1
+Author: Arlin Schaffel
+Author URI: https://github.com/FeXd
+License: MIT
+License URI: https://github.com/FeXd/wordpress-plugin-quick-jobpost/blob/main/LICENSE
+Text Domain: quick-jobpost
+*/
+
+?>
